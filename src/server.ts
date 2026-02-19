033a5f1aa8ae4a5bcd1d604f0f55d48feca3ce0b
aliirtaza7 <airtaza.bscs24seecs@seecs.edu.pk>
2026-02-20 00:37:33 +0500

    Configured database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+  
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
