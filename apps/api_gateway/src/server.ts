ce1f2a9f28488de8c02070d6f0e0d5109240b235
Rohaan Ali <rohaanali763@gmail.com>
2026-04-11 12:24:52 +0500

    server setup for gateway

--- apps/api_gateway/src/server.ts
+++ apps/api_gateway/src/server.ts
@@ -1,10 +1,8 @@
 import app from "./app.js";
-import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
-  await connectDB();
   
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
