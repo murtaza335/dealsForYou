3bfa5d8b510403a19eddf19e1dd46033c2bff5d6
murtaza335 <murtazarasheedblogger@gmail.com>
2026-04-27 00:40:25 +0500

    deals new rroutes added

--- apps/api_gateway/src/routes/deals.routes.ts
+++ apps/api_gateway/src/routes/deals.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 import {
+  getDealById,
+  getDealFilterBrands,
+  getDealFilterCuisineTags,
+  getDealFilterMealTypes,
+  getDealFilterOptions,
+  getDealFilterPriceRange,
   getFilteredDeals,
   getRecommendedDeals,
   getTopDeals,
@@ -8,7 +14,13 @@ import {
 const router = Router();
 
 router.get("/filtered", getFilteredDeals);
+router.get("/filters/options", getDealFilterOptions);
+router.get("/filters/brands", getDealFilterBrands);
+router.get("/filters/cuisine-tags", getDealFilterCuisineTags);
+router.get("/filters/meal-types", getDealFilterMealTypes);
+router.get("/filters/price-range", getDealFilterPriceRange);
 router.get("/recommended", getRecommendedDeals);
 router.get("/top", getTopDeals);
+router.get("/:dealId", getDealById);
 
 export default router;
