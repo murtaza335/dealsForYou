6d188a93a7acd119ff15fab893e1ac577b1b6a1c
Rohaan Ali <rohaanali763@gmail.com>
2026-05-06 13:57:30 +0500

    added favourite apis

--- apps/api_gateway/src/routes/analytics.routes.ts
+++ apps/api_gateway/src/routes/analytics.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { getTrendingDeals, getTrendingBrands, trackEvent } from "../controllers/analyticsController.js";
+import {
+  getTrendingDeals,
+  getTrendingBrands,
+  trackEvent,
+  getFavourites,
+  getFavouritesDetails,
+  addFavourite,
+  removeFavourite,
+} from "../controllers/analyticsController.js";
 import { requireAuth } from "../middlewares/requireAuth.js";
 
 const router = Router();
 router.get("/trending/deals", getTrendingDeals);
 router.get("/trending/brands", getTrendingBrands);
 router.post("/event", trackEvent);
+
+// Favourites routes
+router.get("/favourites", requireAuth, getFavourites);
+router.get("/favourites/details", requireAuth, getFavouritesDetails);
+router.post("/favourites", requireAuth, addFavourite);
+router.delete("/favourites", requireAuth, removeFavourite);
 
 export default router;
